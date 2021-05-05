94c11ecb0d246e1dfbd90a924bfd536705d818d7
kitazaki <kitazaki@gmail.com>
2021-05-05 09:51:12 +0900

    Update fixarm64.js

--- fixarm64.js
+++ fixarm64.js
@@ -4,9 +4,9 @@ var os = require("os");
 var fs = require("fs");
 
 if (os.arch() === "arm64" ) {
-    if (!fs.existsSync('../@tensorflow/tfjs-node/scripts/custom-binary.json')) {
+    if (!fs.existsSync('../../@tensorflow/tfjs-node/scripts/custom-binary.json')) {
         console.log("The custom-binary.json doesn't exist.");
-        fs.copyFileSync('./custom-binary.json','../@tensorflow/tfjs-node/scripts/custom-binary.json');
+        fs.copyFileSync('./custom-binary.json','../../@tensorflow/tfjs-node/scripts/custom-binary.json');
         console.log("The custom-binary.json was copied.");
 	console.log("Please npm install again.");
     } else {
