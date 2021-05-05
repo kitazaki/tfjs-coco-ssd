7a4510087e8185f704baa4e013f61d5ee5ae0011
kitazaki <kitazaki@gmail.com>
2021-05-05 09:45:39 +0900

    Add files via upload

---
+++ fixarm64.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var os = require("os");
+var fs = require("fs");
+
+if (os.arch() === "arm64" ) {
+    if (!fs.existsSync('../@tensorflow/tfjs-node/scripts/custom-binary.json')) {
+        console.log("The custom-binary.json doesn't exist.");
+        fs.copyFileSync('./custom-binary.json','../@tensorflow/tfjs-node/scripts/custom-binary.json');
+        console.log("The custom-binary.json was copied.");
+	console.log("Please npm install again.");
+    } else {
+	console.log("The custom-binary.json exists.");
+    }
+}
